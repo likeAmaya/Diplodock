580a32dbbc7309117ec5617371b0a7fde6268b64
likeAmaya <amida2012@yandex.ru>
2016-06-16 14:57:16 +0300

    Initial bla bla bla

---
+++ OpenGL_on_a_Windows_Form/Form1.h
@@ -0,0 +1,116 @@
+#pragma once
+
+#include "OpenGL.h"
+
+
+namespace OpenGL_on_a_Windows_Form {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+	using namespace OpenGLForm;
+
+	
+
+	/// <summary>
+	/// Summary for Form1
+	///
+	/// WARNING: If you change the name of this class, you will need to change the
+	///          'Resource File Name' property for the managed resource compiler tool
+	///          associated with all .resx files this class depends on.  Otherwise,
+	///          the designers will not be able to interact properly with localized
+	///          resources associated with this form.
+	/// </summary>
+	public ref class Form1 : public System::Windows::Forms::Form
+	{
+	public:
+		Form1(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+			OpenGL = gcnew COpenGL(this, 640, 480);
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~Form1()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::ComponentModel::IContainer^  components;
+	protected: 
+	private: System::Windows::Forms::Timer^  timer1;
+	private: System::Windows::Forms::Button^  button1;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+
+		OpenGLForm::COpenGL ^ OpenGL;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->components = (gcnew System::ComponentModel::Container());
+			this->timer1 = (gcnew System::Windows::Forms::Timer(this->components));
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// timer1
+			// 
+			this->timer1->Enabled = true;
+			this->timer1->Interval = 10;
+			this->timer1->Tick += gcnew System::EventHandler(this, &Form1::timer1_Tick);
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(688, 53);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
+			// 
+			// Form1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(795, 489);
+			this->Controls->Add(this->button1);
+			this->Name = L"Form1";
+			this->Text = L"OpenGL on a Windows Form using Managed C++";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void timer1_Tick(System::Object^  sender, System::EventArgs^  e)
+			 {
+				 UNREFERENCED_PARAMETER(sender);
+				 UNREFERENCED_PARAMETER(e);
+				 OpenGL->Render();
+				 OpenGL->SwapOpenGLBuffers();
+			 }
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+				 UNREFERENCED_PARAMETER(sender);
+				 UNREFERENCED_PARAMETER(e);
+				 // bla bla
+	}
+	};
+}
+
